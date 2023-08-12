07b32549e1f7f8a78ade124c0a007d008ceb7384
Annie <anniebui0110@gmil.com>
2023-08-12 15:09:55 -0500

    new files

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your title of your project',
+                name: 'Title',
+            },
+            {
+                type: 'input',
+                message: 'What is your project about',
+                name: 'Description',
+            },
+            {
+                type: 'input',
+                message: 'What is your installation?',
+                name: 'Installation',
+            },
+            {
+                type: 'input',
+                message: 'What is your usage?',
+                name: 'Usage',
+            },
+            {
+                type: 'list',
+                message: 'What is your license?',
+                choices: ['MIT', 'IBM', 'Apachenod'],
+                name: 'License',
+            },
+            {
+                type: 'input',
+                message: 'What is your contribution',
+                name: 'Contribution',
+            },
+            {
+                type: 'input',
+                message: 'What is your test?',
+                name: 'Test',
+            },
+            {
+                type: 'input',
+                message: 'What is your github url?',
+                name: 'Github',
+            },
+            {
+                type: 'input',
+                message: 'What is your email?',
+                name: 'Email',
+            },
+        ])
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('log.txt', data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    )
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
